616535e394c2e86154052c3052717b256a752a58
vanigami <vanigami@github.com>
2020-04-24 08:53:40 -0400

    initalize git

---
+++ TrainingSessionProject-frontend/src/index.js
@@ -0,0 +1,6 @@
+const BACKEND_URL = 'localhost:3000';
+fetch(`${BACKEND_URL}/test`)
+    .then(response => response.json())
+    .then(parsedResponse => console.log(parsedResponse));
+
+console.log("testing...")
