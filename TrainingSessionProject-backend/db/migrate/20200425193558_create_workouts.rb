27a39124c600b1e888b463204ffc6a3a1374d5c1
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-04-25 15:53:04 -0400

    creates workout table migration

--- TrainingSessionProject-backend/db/migrate/20200425193558_create_workouts.rb
+++ TrainingSessionProject-backend/db/migrate/20200425193558_create_workouts.rb
@@ -1,6 +1,8 @@
 class CreateWorkouts < ActiveRecord::Migration[6.0]
   def change
     create_table :workouts do |t|
+      t.string :name
+      t.text :description
 
       t.timestamps
     end
--- TrainingSessionProject-backend/db/schema.rb
+++ TrainingSessionProject-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_171849) do
+ActiveRecord::Schema.define(version: 2020_04_25_193558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2020_04_25_171849) do
     t.integer "sets"
     t.integer "reps"
     t.integer "duration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
