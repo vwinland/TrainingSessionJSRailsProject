8cf844efb9820cfd79e18b1006a606ba7c232236
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-04-26 13:10:21 -0400

    builds method that will fetch workouts from backend

--- TrainingSessionProject-frontend/src/components/workouts.js
+++ TrainingSessionProject-frontend/src/components/workouts.js
@@ -2,10 +2,11 @@ class Workouts {
     constructor() {
         this.workouts = []
         this.adapter = new WorkoutsAdapter()
-        this.bindEventListeners()
+        //this.bindEventListeners()
         this.fetchAndLoadWorkouts()
     }
 
+    //makes a call to backend api, then displays workouts
     fetchAndLoadWorkouts() {
         this.adapter.getWorkouts().then(workouts => {
             console.log(workouts)
