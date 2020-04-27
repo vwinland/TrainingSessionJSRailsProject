5da8d2e619797443ddcda409aed47ab2abfafa67
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-04-26 20:50:52 -0400

    creates ability to submit new workout name to workout form

--- TrainingSessionProject-frontend/src/components/workouts.js
+++ TrainingSessionProject-frontend/src/components/workouts.js
@@ -9,13 +9,14 @@ class Workouts {
     //dont want to cache the dom every time. invoke workouts constructor that way we dont need to try and always grab dom element each time it renders, it will save it into this property
     initBindingAndEventListeners() {
         this.workoutsContainer = document.getElementById('workouts-container')
+        this.newWorkoutName = document.getElementById('new-workout-name')
         this.workoutForm = document.getElementById('new-workout-form')
-        this.workoutForm.addEventListener('submit', this.createWorkout)
+        this.workoutForm.addEventListener('submit', this.createWorkout.bind(this))//every time form is submitted, make sure to bind the 'this' to notes when we execute createNote
     }
 
     createWorkout(e) {
         e.preventDefault()
-        console.log("workout is being created")
+        console.log(this.newWorkoutName.value)
     }
 
     //makes a call to backend api, then displays workouts
