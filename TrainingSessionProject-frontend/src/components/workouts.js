e6f761aa3064eaf5d08ab6e2a811646936f0825e
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-04-26 20:12:41 -0400

    renders workouts container that iterates through an array from workouts api and displays them in a list

--- TrainingSessionProject-frontend/src/components/workouts.js
+++ TrainingSessionProject-frontend/src/components/workouts.js
@@ -19,9 +19,7 @@ class Workouts {
             })
     }
     render() {
-        const workoutsString = this.workouts.map(workout => `<li>${workout.name}</li>`).join('')
-        console.log(workoutsString)
         const workoutsContainer = document.getElementById('workouts-container')
-
+        workoutsContainer.innerHTML = this.workouts.map(workout => `<li>${workout.name}</li>`).join('')
     }
 }
