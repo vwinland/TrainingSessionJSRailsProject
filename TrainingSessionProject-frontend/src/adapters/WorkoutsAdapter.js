0ef2fbf66316b8cc62f2327296481f080a14dfcc
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-04-29 18:38:14 -0400

    adds an updateWorkout method to adapter that fetches the baseUrl and id of the workout being edited

--- TrainingSessionProject-frontend/src/adapters/WorkoutsAdapter.js
+++ TrainingSessionProject-frontend/src/adapters/WorkoutsAdapter.js
@@ -1,23 +1,36 @@
 class WorkoutsAdapter {
-    constructor() {
-        this.baseUrl = 'http://localhost:3000/api/v1/workouts'
-    }
+  constructor() {
+    this.baseUrl = "http://localhost:3000/api/v1/workouts";
+  }
 
-    getWorkouts() {
-        return fetch(this.baseUrl).then(res => res.json())
-    }
+  getWorkouts() {
+    return fetch(this.baseUrl).then((res) => res.json());
+  }
 
-    createWorkout(value) {
-        const workout = {
-            name: value,
-        }
+  createWorkout(value) {
+    const workout = {
+      name: value,
+    };
 
-        return fetch(this.baseUrl, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify({ workout }),
-        }).then(res => res.json())
-    }
+    return fetch(this.baseUrl, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ workout }),
+    }).then((res) => res.json());
+  }
+
+  updateWorkout(value, id) {
+    const workout = {
+      name: value,
+    };
+    return fetch(`${this.baseUrl}/id`, {
+      method: "PATCH ",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ workout }),
+    }).then((res) => res.json());
+  }
 }
